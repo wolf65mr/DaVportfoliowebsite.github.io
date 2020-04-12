2c36b5d31167b9b029e589ff6608f38556828a9c
wolf65mr <59667783+wolf65mr@users.noreply.github.com>
2020-04-12 21:17:13 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,6 @@
-$(document).ready(function() {
-  function checkWidth() {
-    var windowWidth = $('body').innerWidth(),
-        elem = $(".item-6"); // лучше сохранять объект в переменную, многократно чтобы не насиловать 
-                                    // страницу для поиска нужного элемента
-    if(windowWidth < 720){
-      elem.removeClass('col-6');
-      elem.addClass('col-10');
-    }
-    else{
-      elem.removeClass('col-10');
-      elem.addClass('col-6');
-    }
-  }
-
-  checkWidth(); // проверит при загрузке страницы
-
-  $(window).resize(function(){
-    checkWidth(); // проверит при изменении размера окна клиента
-  });
-});
+function openMenu() {
+  document.getElementById('sidebar').classList.toggle('active');
+  document.getElementById('menu-gamburger').classList.toggle('active');
+  document.getElementById('line').style.background = '#000';
+  document.querySelectorAll('span.line').classList.toggle('bgc');
+}
