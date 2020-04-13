da54869c973ce263d30a9ee58c83c2da183e8d26
wolf65mr <59667783+wolf65mr@users.noreply.github.com>
2020-04-13 15:57:57 +0300

    Delete script.js

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function openMenu() {
-  document.getElementById('sidebar').classList.toggle('active');
-  document.getElementById('menu-gamburger').classList.toggle('active');
-  document.getElementById('line').style.background = '#000';
-  document.querySelectorAll('span.line').classList.toggle('bgc');
-}
